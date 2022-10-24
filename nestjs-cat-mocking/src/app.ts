ade7f15ec30ff24380ee021cf8eb98a8bd54acf7
Kyubum Jang <jkb2221@gmail.com>
2022-10-24 15:44:28 +0900

    feat: singleton pattern 적용

--- nestjs-cat-mocking/src/app.ts
+++ nestjs-cat-mocking/src/app.ts
@@ -1,74 +1,62 @@
 import * as express from 'express';
-import { Cat, CatType } from './app.model';
+import catsRouter from './cats/cats.route';
 
-const app: express.Express = express();
+class Server {
+  public app: express.Application;
 
-interface ApiError {
-  code: string;
-  message: string;
-}
-// logging middleware
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log(req.rawHeaders[1]);
-    console.log('this is logging middleware');
-    next();
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
   }
-);
 
-// json middleware
-app.use(express.json());
-
-// READ 고양이 전체 데이터 다 조회
-app.get('/cats', (req: express.Request, res: express.Response) => {
-  try {
-    const cats = Cat;
-    // throw new Error('db connect error');
-    res.status(200).send({ success: true, data: { cats } });
-  } catch (error: any) {
-    const err = error as ApiError;
-    res.status(400).send({ success: false, error: error.message });
+  private setRoute() {
+    this.app.use(catsRouter);
   }
-});
 
-// READ 특정 고양이 데이터 조회
-app.get('/cats/:id', (req: express.Request, res: express.Response) => {
-  try {
-    const params = req.params;
-    console.log(params);
+  private setMiddleware() {
+    // logging middleware
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        console.log(req.rawHeaders[1]);
+        console.log('this is logging middleware');
+        next();
+      }
+    );
 
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
+    // json middleware
+    this.app.use(express.json());
+
+    this.setRoute();
+
+    // 404 middleware
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        console.log(req.rawHeaders[1]);
+        res.send({ error: '404 not found error' });
+        next();
+      }
+    );
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log('server is on 8000 port');
     });
-    res.status(200).send({ success: true, data: { cat } });
-  } catch (error: any) {
-    const err = error as ApiError;
-    res.status(400).send({ success: false, error: error.message });
   }
-});
+}
 
-// CREATE 새로운 고양이 추가
-app.post('/cats', (req: express.Request, res: express.Response) => {
-  try {
-    const data = req.body;
-    console.log(data);
-    Cat.push(data); //CREATE
-    res.status(200).send({ success: true, data: { data } });
-  } catch (error: any) {
-    const err = error as ApiError;
-    res.status(400).send({ success: false, error: error.message });
-  }
-});
+function init() {
+  const server = new Server();
+  server.listen();
+}
 
-// 404 middleware
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log(req.rawHeaders[1]);
-    res.send({ error: '404 not found error' });
-    next();
-  }
-);
-
-app.listen(8000, () => {
-  console.log('server is on 8000 port');
-});
+init();
---
+++ nestjs-cat-mocking/src/cats/cats.route.ts
@@ -0,0 +1,107 @@
+import * as express from 'express';
+
+import { Cat, CatType } from './cats.model';
+import { Router } from 'express';
+
+const router = Router();
+
+interface ApiError {
+  code: string;
+  message: string;
+}
+
+// READ 고양이 전체 데이터 다 조회
+router.get('/cats', (req: express.Request, res: express.Response) => {
+  try {
+    const cats = Cat;
+    // throw new Error('db connect error');
+    res.status(200).send({ success: true, data: { cats } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// READ 특정 고양이 데이터 조회
+router.get('/cats/:id', (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    console.log(params);
+
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({ success: true, data: { cat } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// CREATE 새로운 고양이 추가
+router.post('/cats', (req: express.Request, res: express.Response) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data); //CREATE
+    res.status(200).send({ success: true, data: { data } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// UPDATE 고양이 데이터 업데이트 (PUT)
+router.put('/cats/:id', (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+
+    res.status(200).send({ success: true, data: { result } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// UPDATE 고양이 데이터 부분적으로 업데이트 (Patch)
+router.patch('/cats/:id', (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+
+    res.status(200).send({ success: true, data: { result } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// DELETE 고양이 데이터 삭제 (Delete)
+router.delete('/cats/:id', (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+
+    res.status(200).send({ success: true, data: newCat });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+export default router;
