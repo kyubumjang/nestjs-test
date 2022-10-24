3e6725092f93e35021c24a7a844fbff7b912300e
Kyubum Jang <jkb2221@gmail.com>
2022-10-24 13:24:36 +0900

    feat: middleware 추가

--- nestjs-cat-mocking/src/app.ts
+++ nestjs-cat-mocking/src/app.ts
@@ -3,23 +3,49 @@ import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
 
+// 미들웨어
+// next 함수: 프론트 => 미들웨어를 거치고 next 함수로 라우터를 찾음, 찾아서 실행
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    console.log('this is logging middleware');
+    next();
+  }
+);
+
+app.get(
+  '/cats/som',
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    console.log('this is som middleware');
+    next();
+  }
+);
+
+// 라우터
 app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
   res.send({ cats: Cat });
 });
 
 app.get(
   '/cats/blue',
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log(req.rawHeaders[1]);
     res.send({ blue: Cat[0] });
   }
 );
 
 app.get('/cats/som', (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
   res.send({ som: Cat[1] });
 });
+
+// end-point 일치하는 것이 없을 때
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    res.send({ error: '404 not found error' });
+    next();
+  }
+);
 
 app.listen(8000, () => {
   console.log('server is on 8000 port');
