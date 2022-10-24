e67497b9d5a823b85537ac887afd9163f5aa389e
Kyubum Jang <jkb2221@gmail.com>
2022-10-24 14:07:57 +0900

    feat: Create, Read 테스팅

--- nestjs-cat-mocking/src/app.ts
+++ nestjs-cat-mocking/src/app.ts
@@ -3,8 +3,11 @@ import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
 
-// 미들웨어
-// next 함수: 프론트 => 미들웨어를 거치고 next 함수로 라우터를 찾음, 찾아서 실행
+interface ApiError {
+  code: string;
+  message: string;
+}
+// logging middleware
 app.use(
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log(req.rawHeaders[1]);
@@ -13,32 +16,51 @@ app.use(
   }
 );
 
-app.get(
-  '/cats/som',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log(req.rawHeaders[1]);
-    console.log('this is som middleware');
-    next();
-  }
-);
+// json middleware
+app.use(express.json());
 
-// 라우터
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
+// READ 고양이 전체 데이터 다 조회
+app.get('/cats', (req: express.Request, res: express.Response) => {
+  try {
+    const cats = Cat;
+    // throw new Error('db connect error');
+    res.status(200).send({ success: true, data: { cats } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
 });
 
-app.get(
-  '/cats/blue',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send({ blue: Cat[0] });
-  }
-);
+// READ 특정 고양이 데이터 조회
+app.get('/cats/:id', (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    console.log(params);
 
-app.get('/cats/som', (req: express.Request, res: express.Response) => {
-  res.send({ som: Cat[1] });
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({ success: true, data: { cat } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
 });
 
-// end-point 일치하는 것이 없을 때
+// CREATE 새로운 고양이 추가
+app.post('/cats', (req: express.Request, res: express.Response) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data); //CREATE
+    res.status(200).send({ success: true, data: { data } });
+  } catch (error: any) {
+    const err = error as ApiError;
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
+// 404 middleware
 app.use(
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log(req.rawHeaders[1]);
