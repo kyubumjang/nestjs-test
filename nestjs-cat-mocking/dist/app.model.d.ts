1e28156082ffaf3e02b7521c0750bdeec800d522
Kyubum Jang <jkb2221@gmail.com>
2022-10-24 13:22:59 +0900

    delete: dist 폴더 삭제

--- nestjs-cat-mocking/dist/app.model.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type CatType = {
-    id: string;
-    name: string;
-    age: number;
-    species: string;
-    isCute: boolean;
-    friends: string[];
-};
-export declare const Cat: CatType[];
