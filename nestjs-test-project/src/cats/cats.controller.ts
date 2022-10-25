f840d85e864e94d344a24b4e2efea9454b8a6052
Kyubum Jang <jkb2221@gmail.com>
2022-10-25 23:46:58 +0900

    feat: cats 모듈, 컨트롤러, 서비스 추가

---
+++ nestjs-test-project/src/cats/cats.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update partial cat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete cat';
+  }
+}
