c8fd77186b21de5c9d6af49622f86ee5f969bb6a
Kyubum Jang <jkb2221@gmail.com>
2022-10-26 10:18:52 +0900

    feat: interceptor 구현 및 적용

--- nestjs-test-project/src/cats/cats.controller.ts
+++ nestjs-test-project/src/cats/cats.controller.ts
@@ -9,25 +9,31 @@ import {
   Post,
   Put,
   UseFilters,
+  UseInterceptors,
 } from '@nestjs/common';
-import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { CatsService } from './cats.service';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCat() {
-    throw new HttpException('api is broken', 401);
-    return 'all cat';
+    console.log('cat controller');
+
+    return { cats: 'get all cat api' };
   }
 
   @Get(':id')
-  getOneCat(@Param('id', ParseIntPipe) param: number) {
-    console.log(param);
-    console.log(typeof param);
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+    // console.log(param);
+    // console.log(typeof param);
+
     return 'one cat';
   }
 
