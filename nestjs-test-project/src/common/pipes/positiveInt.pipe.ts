df6aec177ea23fbbb76cfb0e6bc05e85f37b7996
Kyubum Jang <jkb2221@gmail.com>
2022-10-26 10:19:08 +0900

    chore: Project 구조 변경

---
+++ nestjs-test-project/src/common/pipes/positiveInt.pipe.ts
@@ -0,0 +1,13 @@
+import { HttpException, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class PositiveIntPipe implements PipeTransform {
+  transform(value: number) {
+    console.log(value);
+    if (value < 0) {
+      throw new HttpException('value > 0', 400);
+    }
+
+    return value;
+  }
+}
