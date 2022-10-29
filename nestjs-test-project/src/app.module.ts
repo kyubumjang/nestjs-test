c122f5530509ce1da0d8840d96d254829a6492aa
Kyubum Jang <jkb2221@gmail.com>
2022-10-29 18:52:20 +0900

    feat: mongoose 추가 및 mongoDB 연결

--- nestjs-test-project/src/app.module.ts
+++ nestjs-test-project/src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
