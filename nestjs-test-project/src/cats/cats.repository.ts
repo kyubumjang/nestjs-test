76965b08229e884d9c0af6aa32428f293864ecfe
Kyubum Jang <jkb2221@gmail.com>
2022-10-31 02:29:55 +0900

    feat: Repository 패턴 적용, DB 직접 접근 코드 삭제

---
+++ nestjs-test-project/src/cats/cats.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  /**
+   * 이렇게 직접적으로 에러처리도 할 수 있음. 현재는 mongoose로 validation 체크를 처리하고 있음
+   * try catch로 error 발생시 HttpException('db error, 400)
+   */
+  async existsByEmail(email: string): Promise<{ _id: any }> {
+    const result = await this.catModel.exists({ email });
+    return result;
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
